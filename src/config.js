ec14dab2b4472a62acaa5b294ab2216ebf04b163
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2024-03-04 23:42:12 -0300

    fix token

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
 config();
-export const TOKEN_SECRET = "token key";
+export const TOKEN_SECRET = "Mijwtsecretapi2024";
 export const URL_LOCALHOST2 = 'http://localhost:5173';
 export const PORT = process.env.PORT || 3002;
